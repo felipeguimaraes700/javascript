77bc1b52b182c8ef0aa47a78778b883484923d6a
Felipe Guimarães <felipe700guima@gmail.com>
2022-12-23 21:23:18 -0300

    estilizando o projeto

---
+++ mini-projetos/calcularIMC/javascript.js
@@ -0,0 +1,6 @@
+let name = document.querySelector('#name')
+let heigth = document.querySelector('#heigth')
+let weigth = document.querySelector('#weigth')
+let botton = document.querySelector('botton')
+let resultado = document.querySelector('#resultado')
+
