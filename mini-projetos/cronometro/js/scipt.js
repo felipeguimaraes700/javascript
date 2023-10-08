2efdf07a95be05d43aa90df2672c9b64d29cdd2a
Felipe Guimarães <felipe700guima@gmail.com>
2023-10-08 13:29:03 -0300

    criando estrutura do projeto

---
+++ mini-projetos/cronometro/js/scipt.js
@@ -0,0 +1,13 @@
+const minutsE1 = document.querySelector('#minutos')
+const secundsE1 = document.querySelector('#segundos')
+const milisecundsE1 = document.querySelector('#milesegundos')
+const iniciar = document.querySelector('#iniciar')
+const pausar = document.querySelector('#pausar')
+const continuar = document.querySelector('#continuar')
+const resetar = document.querySelector('#resetar')
+
+let minuts = 0
+let secunds = 0
+let milisecunds = 0
+let ispaused = false
+
